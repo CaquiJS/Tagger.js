0f70bd608f6bce57ad5549bce78a8e8f34399d83
Renan Vaz <renan.c.vaz@gmail.com>
2015-02-03 13:42:49 -0200

    Working

--- js/tagger.js
+++ js/tagger.js
@@ -1,4 +1,45 @@
 (function($){
+
+    function Tagger(el, settings) {
+        var $el, $ul, $li, $input, $parent, tagTpl;
+
+        tagTpl      = '<li class="tagger-item-label">'+settings.tagTpl+'</li>';
+
+        $el         = $(el);
+
+        $ul         = $('<ul id="tagger-'+$el.attr('id')+'" class="tagger"><li class="tagger-item-input"></li></ul>');
+        $li         = $ul.find('.tagger-item-input');
+
+        $parent     = $el.parent();
+        $input      = $el.detach();
+
+        $li.append($input);
+
+        $input.on('keypress', function(e) {
+            if(e.which === 13) {
+                $li.before($(tagTpl.replace('{{ title }}', 'Test')));
+            }
+        });
+
+        $parent.append($ul);
+
+        var _list = function() {
+
+        };
+
+        this.add = function() {
+
+        };
+
+        this.remove = function() {
+
+        };
+
+        this.destroy = function() {
+
+        };
+    }
+
     /**
      * Tagger
      * @param  Object params Config params
@@ -16,32 +57,14 @@
             var defaults = {
                 tags:       [], // or function
                 minDigits:  3,
-                maxTags:    false
+                maxTags:    false,
+                tagTpl      = '<span class="tagger-item-label-text">{{ title }}</span><button class="tagger-item-label-close">X</button>';
             };
 
             var settings = $.extend(defaults, params);
 
             return $(this).each(function(){
-                var $this, $ul, $li, $input, $parent, tagTpl, count = 0;
 
-                tagTpl      = '<li class="tagger-item-label"><span class="tagger-item-label-text">{{ title }}</span><button class="tagger-item-label-close">X</button></li>';
-
-                $this       = $(this);
-                $ul         = $('<ul id="tagger-'+$this.attr('id')+'" class="tagger"><li class="tagger-item-input"></li></ul>');
-                $li         = $ul.find('.tagger-item-input');
-
-                $parent     = $this.parent();
-                $input      = $this.detach();
-
-                $li.append($input);
-
-                $input.on('keypress', function(e) {
-                    if(e.which === 13) {
-                        $li.before($(tagTpl.replace('{{ title }}', 'Test '+(count++))));
-                    }
-                });
-
-                $parent.append($ul);
             });
         } else {
             return $(this).each(function(){
